bc3c627329a1b81ed298528e4330bd688ba9c90c
Yukiichan97 <112150469+Yukiichan97@users.noreply.github.com>
2024-03-09 19:16:28 +0700

    thuc hanh

--- Ex3_SimpleSumApp/app/src/main/java/minh/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/minh/edu/ex3_simplesumapp/MainActivity.java
@@ -11,33 +11,88 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText ketQua;
-    Button btnTong;
+public class MainActivity extends AppCompatActivity{
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnTong = (Button) findViewById(R.id.btnTong);
-        btnTong.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.btnTong){
-            int a = Integer.parseInt(((EditText)findViewById(R.id.txtA)).getText().toString());
-            int b = Integer.parseInt(((EditText)findViewById(R.id.txtB)).getText().toString());
-            ketQua = (EditText) findViewById(R.id.txtKetQua);
-            ketQua.setText(String.valueOf(a+b));
-        }
+        TimDieuKien();
     }
 
+    void TimDieuKien(){
+        editTextSo1 = (EditText) findViewById(R.id.txtA);
+        editTextSo2 = (EditText) findViewById(R.id.txtB);
+        editTextKetQua = (EditText) findViewById(R.id.txtKetQua);
+        Button btnCong = (Button) findViewById(R.id.btnCong);
+        Button btnTru = (Button) findViewById(R.id.btnTru);
+        Button btnNhan = (Button) findViewById(R.id.btnNhan);
+        Button btnChia = (Button) findViewById(R.id.btnChia);
+        btnCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyCong(v);
+            }
+        });
+        btnTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XyLyTru(v);
+            }
+        });
+        btnNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyNhan(v);
+            }
+        });
+        btnChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyChia(v);
+            }
+        });
+    }
+
+    void XuLyCong(View view){
+
+        float a = Float.parseFloat(editTextSo1.getText().toString());
+        float b = Float.parseFloat(editTextSo2.getText().toString());
+        float c = a + b;
+
+
+        String chuoiKQ = String.valueOf(c);
+        editTextKetQua.setText(chuoiKQ);
+    }
+
+    void XyLyTru(View view){
+
+        float a = Float.parseFloat(editTextSo1.getText().toString());
+        float b = Float.parseFloat(editTextSo2.getText().toString());
+        float c = a - b;
+
+        String chuoiKQ = String.valueOf(c);
+        editTextKetQua.setText(chuoiKQ);
+    }
+
+    void XuLyNhan(View view){
+
+        float a = Float.parseFloat(editTextSo1.getText().toString());
+        float b = Float.parseFloat(editTextSo2.getText().toString());
+        float c = a * b;
+        String chuoiKQ = String.valueOf(c);
+        editTextKetQua.setText(chuoiKQ);
+    }
+
+    void XuLyChia(View view) {
+
+        float a = Float.parseFloat(editTextSo1.getText().toString());
+        float b = Float.parseFloat(editTextSo2.getText().toString());
+        float c = a / b;
+        String chuoiKQ = String.valueOf(c);
+        editTextKetQua.setText(chuoiKQ);
+    }
 }
